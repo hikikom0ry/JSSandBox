3b5a251c150d32c4c414c728463e6581388533c8
Zaur <ironetz@rambler.ru>
2022-04-26 18:14:47 +0300

    3-3

--- JSLearning/Sandbox/js/script.js
+++ JSLearning/Sandbox/js/script.js
@@ -1,3 +1,28 @@
 'use strict';
 
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(1));
+// console.log(btns[1].classList.add('red'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
 
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
+
+// btns[0].addEventListener('click', (e) => {
+//     btns[1].classList.toggle('red');
+// });
+
+wrapper.addEventListener('click', (e) => {
+    if(e.target && e.target.tagName == 'BUTTON') {
+        console.log(e.target);
+        btns[1].classList.toggle('red');
+    }
+});
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
