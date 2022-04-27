0ab33cdad6e5d16a16d0812e122d7bddc6fd83d7
Zaur <ironetz@rambler.ru>
2022-04-27 11:46:20 +0300

    3-3 mod

--- JSLearning/Food/js/script.js
+++ JSLearning/Food/js/script.js
@@ -1,31 +1,30 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
+          tabsParent = document.querySelector('.tabheader__items'),
+          tabsContent = document.querySelectorAll('.tabcontent');
 
     function hideTabContent() {
         tabsContent.forEach(item => {
             item.classList.remove('show', 'fade');
             item.classList.add('hide');
-        });
 
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
+            tabs.forEach(item => {
+                item.classList.remove('tabheader__item_active');
+            });
         });
     }
 
     function showTabContent(i = 0) {
-        tabsContent[i].classList.remove('hide');
-        tabsContent[i].classList.add('show', 'fade');
         tabs[i].classList.add('tabheader__item_active');
-    }
-    hideTabContent();
-    showTabContent();
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
 
-    tabsParent.addEventListener('click', (event) => {
+    }
+
+    tabsParent.addEventListener('click', event => {
         const target = event.target;
-        if (target && target.classList.contains('tabheader__item')) {
+        if (target && target.classList.contains('tabheader__item')){
             tabs.forEach((item, i) => {
                 if (target == item) {
                     hideTabContent();
@@ -33,6 +32,11 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
             });
         }
+        
     });
+
+    hideTabContent();
+    showTabContent();
+
 });
 
