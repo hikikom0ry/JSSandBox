2fa077ee53435187e6d40313661d8436e18c6453
Zaur <ironetz@rambler.ru>
2022-05-03 14:16:37 +0300

    3-8

--- JSLearning/Food/js/script.js
+++ JSLearning/Food/js/script.js
@@ -90,6 +90,40 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     setClock(deadline, '.timer');
+
+    //modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach((item) => {
+        item.addEventListener('click', event => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = 'auto';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', event => {
+        if (event.code == "Escape" && modal.classList.contains('show')) {
+            closeModal(); 
+        }
+    });
     
 });
 
