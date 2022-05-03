f163e3bf51030821fe976612dcb6f5524c03795d
Zaur <ironetz@rambler.ru>
2022-05-03 12:05:15 +0300

    3-7

--- JSLearning/Sandbox/script.js
+++ JSLearning/Sandbox/script.js
@@ -8,13 +8,21 @@ const box = document.querySelector('.box');
 // const width = box.offsetWidth;
 // const height = box.offsetHeight;
 
-const width = box.scrollWidth;
-const height = box.scrollHeight;
+// const width = box.scrollWidth;
+// const height = box.scrollHeight;
 
-console.log(width, height);
+// console.log(width, height);
 
 document.querySelector('button').addEventListener('click', () => {
+    // box.style.height = height + 'px';
+    // console.log(box.scrollTop);
+    console.log(document.documentElement.clientWidth);
     
 });
 
+// console.log(box.getBoundingClientRect().top);
 
+// const style = window.getComputedStyle(box);
+// console.log(style.display);
+
+// console.log(document.documentElement.clientWidth);
